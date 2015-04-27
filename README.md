951ee1e46f93ca4628d09794be61038493ed0a90
Kim Dang & Toby Alden <student@epicodus.com>
2015-04-27 12:25:02 -0700

    Add info in README.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Triangles
+
+This app determines if a combination of three side lengths is an equilateral, isosceles, or scalene triangle (or not a triangle at all!). 
--- spec/triangles_integration_spec.rb
+++ spec/triangles_integration_spec.rb
@@ -4,7 +4,7 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_expectations,false)
 
-describe('the path to the traingles page', {:type => :feature}) do
+describe('the path to the triangles page', {:type => :feature}) do
   it('presents a form to input three side lengths of a triangle') do
     visit('/')
     fill_in('side1', :with => '5')
